4e1a41697b3de7e1778c5d83ace9d065620743e8
Sezayi <arslan.sezayi@gmail.com>
2019-07-26 10:56:51 +0200

    first commit

--- src/pages/SignUpForm.js
+++ src/pages/SignUpForm.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 class SignUpForm extends Component {
     constructor() {
@@ -7,13 +6,14 @@ class SignUpForm extends Component {
 
         this.state = {
             email: '',
-            password: '',
             name: '',
             hasAgreed: false
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.inputNameRef = React.createRef()
     }
 
     handleChange(e) {
@@ -33,31 +33,32 @@ class SignUpForm extends Component {
         console.log(this.state);
     }
 
+    componentDidMount(){
+      this.inputNameRef.current.focus()
+    }
+
     render() {
         return (
         <div className="FormCenter">
+            <div className="FormSubHeaderTitle">Download it for free</div>
+            <div className="FormHeaderTitle"> The SME purpose Ebook</div>
             <form onSubmit={this.handleSubmit} className="FormFields">
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="name">Full Name</label>
-                <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name" value={this.state.name} onChange={this.handleChange} />
+                <input ref={this.inputNameRef} type="text" id="name" className="FormField__Input" placeholder="John Good" name="name" value={this.state.name} onChange={this.handleChange} />
               </div>
               <div className="FormField">
-                <label className="FormField__Label" htmlFor="password">Password</label>
-                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
+                <label className="FormField__Label" htmlFor="name">Your work e-mail</label>
+                <input type="email" id="email" className="FormField__Input" placeholder="you@yourwork.com" name="email" value={this.state.email} onChange={this.handleChange} />
               </div>
-              <div className="FormField">
-                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
-              </div>
-
               <div className="FormField">
                 <label className="FormField__CheckboxLabel">
-                    <input className="FormField__Checkbox" type="checkbox" name="hasAgreed" value={this.state.hasAgreed} onChange={this.handleChange} /> I agree all statements in <a href="" className="FormField__TermsLink">terms of service</a>
+                    <input className="FormField__Checkbox" type="checkbox" name="hasAgreed" required={true} value={this.state.hasAgreed} onChange={this.handleChange} /> I agree to all statements in the <a href="" className="FormField__TermsLink">terms of service</a>
                 </label>
               </div>
 
               <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign Up</button> <Link to="/sign-in" className="FormField__Link">I'm already member</Link>
+                  <button className="FormField__Button">Download E-book</button>
               </div>
             </form>
           </div>
