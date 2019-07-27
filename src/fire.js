9c27b5b8a8035f58b8b965d0aaab6d69e158ac2b
Sezayi <arslan.sezayi@gmail.com>
2019-07-27 20:18:54 +0200

    add firebase

---
+++ src/fire.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'  
+  
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDQlA5o4mFhGxG90gFJrZYADDlSI6QO_Wk",
+    authDomain: "conversion-landingpage.firebaseapp.com",
+    databaseURL: "https://conversion-landingpage.firebaseio.com",
+    projectId: "conversion-landingpage",
+    storageBucket: "",
+    messagingSenderId: "587238548039",
+    appId: "1:587238548039:web:8d7fb89f075322bf"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default fire;
