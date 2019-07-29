c4ef30dadf3e09c9b51c66e858a58ef234d234c2
Sezayi Arslan <arslan.sezayi@gmail.com>
2019-07-29 17:38:04 +0200

    first test with flexbox-grid

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import SignUpForm from './components/SignUpForm';
 import ThankYou from './components/ThankYou';
 import Ebook from './images/ebook.png';
 import './App.css';
+import { Grid, Row, Col} from 'react-flexbox-grid';
 
 class App extends Component {
   render() {
     return (
       <Router basename="/">
-          <div className="App">
-            <div className="App__Form">
-                <Route exact path="/" component={SignUpForm}>
-                </Route>
-                <Route path="/thankyou" component={ThankYou}>
-                </Route>
-            </div>
-            <div className="App__Aside">
-              <div className="ImageWrapper">
-              <img className="ImageStyle" src={Ebook} alt="ebook"/>
-              </div>
-            </div>          
-          </div>
+        <Grid fluid>
+            <Row>
+                  <div className="ImageWrapper">
+                  <img className="ImageStyle" src={Ebook} alt="ebook"/>
+                  </div>   
+            </Row>
+            <Row>
+                  <Route exact path="/" component={SignUpForm}>
+                  </Route>
+                  <Route path="/thankyou" component={ThankYou}>
+                  </Route>
+            </Row>
+          </Grid>
       </Router>
     );
   }
